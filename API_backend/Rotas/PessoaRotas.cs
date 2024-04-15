234ef885b43b08c21c58a684e53bbabd1969c05b
Jonas da Silva Rabelo <130485427+JonasRabelo@users.noreply.github.com>
2024-04-15 14:06:49 -0300

    Projeto api dotnet consumida com angular

---
+++ API_backend/Rotas/PessoaRotas.cs
@@ -0,0 +1,38 @@
+﻿using API_backend.Models;
+
+namespace API_backend.Rotas
+{
+    public static class PessoaRotas
+    {
+        public static List<Pessoa> Pessoas = new List<Pessoa>() {
+            new Pessoa(Guid.NewGuid(), "Neymar"),
+            new Pessoa(Guid.NewGuid(), "Cristiano"),
+            new Pessoa(Guid.NewGuid(), "Messi")
+        };
+
+        public static void MapPessoaRotas(this WebApplication app)
+        {
+            app.MapGet("/pessoas", () => Pessoas);
+            app.MapGet("/pessoas/{nome}", (String nome) => Pessoas.Find(x => x.Nome.StartsWith(nome)));
+
+            app.MapPost("/pessoas", (Pessoa pessoa) => {
+                pessoa.Id = Guid.NewGuid();
+                Pessoas.Add(pessoa);
+                return Results.Ok(pessoa);
+            });
+
+            app.MapPut("/pessoas/{id}", (Guid id, Pessoa pessoa) => {
+                var encontrado = Pessoas.Find(x => x.Id == id);
+
+                if (encontrado == null)
+                    return Results.NotFound();
+
+                encontrado.Nome = pessoa.Nome;
+
+                return Results.Ok(encontrado);
+               
+
+            });
+        }
+    }
+}
